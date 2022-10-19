0587b708dc7f6cdaddf97176460d7f01721a5554
Willian Brun <willian.brun@stara.com.br>
2022-10-18 21:50:07 -0300

    Minor fixes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -19,11 +19,13 @@ function Header() {
                     </Navbar.Toggle>
 
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link to="/departament" as={Link}>Departamento</Nav.Link>
-                        <Nav.Link to="/category" as={Link}>Categorias</Nav.Link>
-                        <Nav.Link to="/patrimony" as={Link}>Patrimônios</Nav.Link>
-                        <Navbar.Text className="divisor" />
-                        <Nav.Link href="/">Sair</Nav.Link>
+                        <Nav>
+                            <Nav.Link to="/departament" as={Link}>Departamento</Nav.Link>
+                            <Nav.Link to="/category" as={Link}>Categorias</Nav.Link>
+                            <Nav.Link to="/patrimony" as={Link}>Patrimônios</Nav.Link>
+                            <Navbar.Text className="divisor" />
+                            <Nav.Link href="/">Sair</Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
